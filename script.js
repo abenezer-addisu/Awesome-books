3e0e5b1376a1c83448a6043a923f98a600a0feea
iamalex <ngashalex8@gmail.com>
2023-05-01 16:39:40 +0300

    added javascript code

---
+++ script.js
@@ -0,0 +1,72 @@
+//checking if local storage is empty
+
+if (localStorage.getItem('Added Books') == null){
+    localStorage.setItem('Added Books', JSON.stringify([]));
+}
+
+//Store data into local storage
+
+const storeData = JSON.parse(localStorage.getItem('Added Books'));
+
+function updateData(){
+    localStorage.setItem('Added Books', JSON.stringify(storeData));
+}
+
+// Getting values from the input fields
+
+const form = document.getElementById('form');
+
+const addBtn = document.querySelector('.add-btn');
+
+addBtn.addEventListener('click', (e) => {
+    const title = document.querySelector('.title');
+    const author = document.querySelector('.author');
+    e.preventDefault;
+    addNewdata(title.value, author.value);
+});
+
+function createBooks(arr){
+    let books = '';
+
+    for (let i = 0; i < arr.length; i+=1){
+        books += ` 
+        <p>${arr[i].title}</p>
+        <p>${arr[i].author}</p>
+        <button onclick ="removeBook(${i})">Remove</button>
+        <hr/>
+        `;
+    }
+    return books;
+}
+
+//displaying data t the UI from local storage
+
+function displayBooks() {
+    const listOfBooks = document.querySelector('.container');
+    listOfBooks.innerHTML = `
+    <ul class="book-ul">
+    ${createBooks(storeData)}</ul>
+    `
+};
+// adding data inthe local storage
+
+function addNewdata(bookTitle, bookAuthor){
+    const Book = {
+        title:bookTitle,
+        author:bookAuthor
+    };
+    storeData.push(Book);
+    updateData();
+    displayBooks();
+}
+
+//removing data from local storage
+
+function removeBook(i) {
+    storeData.splice(i, 1);
+    updateData();
+    displayBooks();
+}
+removeBook();
+
+window.onload = displayBooks();
